67cec2378205f410ea55a81c91beccc01e3b04a2
Rajapazza <nadeeshanrj05@gmail.com>
2024-12-19 17:11:59 +0530

    Changed download directory

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
 import time
+import os
+
+#Difine New Download Folder
+download_dir = r"C:\Users\Rajapazza\Downloads\pyhon_test"
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+else:
+    print("Folder Located")
+
+#Set download preferences to specify the download folder
+prefs = {
+    "download.default_directory": download_dir,
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+}
 
 #Automatically Browser Closes Error Fix
 options = webdriver.ChromeOptions() 
 options.add_experimental_option('detach', True)
+options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(options=options)
+
+
 
 #Site URL
 driver.get('https://fitgirl-repacks.site/grand-theft-auto-v/') 
@@ -30,45 +49,42 @@ for link in links:
 sorted_parts = [part[1] for part in sorted(parts)]
 
 # Print the sorted parts
-#for part in sorted_parts:
-    #print(part)
+for part in sorted_parts:
+    print(part)
 
 for parts in links:
     parts.click()
-    #print("Clicked")
+    driver.switch_to.window(driver.window_handles[1])
+    print("Now In 2nd Page")
+    time.sleep(10)
+    download_bt = driver.find_elements(By.ID, 'method_free')
+    if download_bt:
+        download_bt[0].click()
+    else:
+        print("Download button not found")
+
+    driver.switch_to.window(driver.window_handles[2])
+    try:
+        driver.switch_to.window(driver.window_handles[2])
+        driver.close()
+        print("Page closed")
+    except IndexError:
+        print("No page found")
+
+    driver.switch_to.window(driver.window_handles[1])
+    button = driver.find_element(By.XPATH, "//button[@type='submit' and contains(@class, 'bg-blue-600')]")
+    button.click()
+
+    try:
+        driver.switch_to.window(driver.window_handles[2])
+        driver.close()
+        print("Page closed")
+    except IndexError:
+        print("No page found")
+
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(5)
+    buttondl = driver.find_element(By.XPATH, "//button[@type='submit' and contains(@class, 'bg-blue-600')]")
+    buttondl.click()
+
     break
-
-driver.switch_to.window(driver.window_handles[1])
-print("Now In 2nd Page")
-time.sleep(10)
-download_bt = driver.find_elements(By.ID, 'method_free')
-if download_bt:
-    download_bt[0].click()
-else:
-    print("Download button not found")
-
-driver.switch_to.window(driver.window_handles[2])
-try:
-    driver.switch_to.window(driver.window_handles[2])
-    time.sleep(5)
-    driver.close()
-    print("Page closed")
-except IndexError:
-    print("No page found")
-
-driver.switch_to.window(driver.window_handles[1])
-button = driver.find_element(By.XPATH, "//button[@type='submit' and contains(@class, 'bg-blue-600')]")
-button.click()
-
-try:
-    driver.switch_to.window(driver.window_handles[2])
-    time.sleep(5)
-    driver.close()
-    print("Page closed")
-except IndexError:
-    print("No page found")
-
-driver.switch_to.window(driver.window_handles[1])
-time.sleep(5)
-buttondl = driver.find_element(By.XPATH, "//button[@type='submit' and contains(@class, 'bg-blue-600')]")
-buttondl.click()
